2a5e43c4ddc19c7467e62c35c72a6b233b4417c6
muskiii <josemariafabiano@gmail.com>
2018-10-24 18:05:43 -0300

    add index

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const TOKEN = "NDg5ODU2NDc4OTc5NTU1MzI5.DnxBwg.4VJt_GYuzXcMc1zngU7imsQ8aWQ";
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(process.env.PORT || 5000, function () {
