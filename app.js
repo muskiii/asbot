119284e6278cc41775ea446b373a5db25803fe80
aspengs <jmf@grayshirts.com.ar>
2019-05-22 15:10:23 -0300

    axios

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-'use strict';
 const botClient = require('./services/botClient').botClient;
 var express = require('express');
 var path = require('path');
 
 const TOKEN = "NDg5ODU2NDc4OTc5NTU1MzI5.DnxBwg.4VJt_GYuzXcMc1zngU7imsQ8aWQ";
 
+let port =  process.env.PORT || 5000;
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
-
-app.get('/', function (req, res) {
-  res.send('AsBot Madafaka');
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-let port =  process.env.PORT || 5000;
 app.listen( 5000, function () {
   console.log('App is up and running over port :' + port);
 });
--- services/botClient.js
+++ services/botClient.js
@@ -1,160 +1,177 @@
-'use strict';
-const commando = require('discord.js-commando');
-const axios = require('axios');
-const YTDL = require('ytdl-core');
-const survivClient = require('./survivClient').survivClient;
+"use strict";
+const commando = require("discord.js-commando");
+const axios = require("axios");
+const YTDL = require("ytdl-core");
+const survivClient = require("./survivClient").survivClient;
 
 const PREFIX = ">";
 
 var servers = {};
-var botClient  = {};
-botClient.bot =  new commando.Client();
+var botClient = {};
+botClient.bot = new commando.Client();
 
-botClient.bot.on('message', (message) => {
-    global.myMessage = message;
+botClient.bot.on("message", message => {
+  global.myMessage = message;
 
-    if (message.author.equals(botClient.bot.user)) return;
-    if (!message.content.startsWith(PREFIX)) return;
+  if (message.author.equals(botClient.bot.user)) return;
+  if (!message.content.startsWith(PREFIX)) return;
 
-    var args = message.content.substring(PREFIX.length).split(" ");
+  var args = message.content.substring(PREFIX.length).split(" ");
 
-    switch (args[0].toLowerCase()) {
-        case "light":
-         return toggleLight().then(function(){
-             message.send.channel("done");
-         });
-        case "nasa":
-            return getNasaStuff().then(function(data){
-                console.log(data);
-                message.channel.send({embed: {
-                    color: 3447003,                   
-                    title: data.title,
-                    image: {
-                        url: data.hdurl
-                      },
-                    description: data.explanation,
-                    timestamp: new Date(),
-                    footer: {
-                      text: "Aspen Rules"
-                    }
-                  }
-                });
-            });
-
-        case "ip": 
-            return myIp().then(function(result){
-
-                console.log(result.data.ip);
-                message.author.send(result.data.ip);
-            });
-        case "door": 
-        return openDoor().then(function(){
-            message.channel.send("Abrete Sésamo");
+  switch (args[0].toLowerCase()) {
+    case "light":
+      return toggleLight().then(function() {
+        message.send.channel("done");
+      });
+    case "nasa":
+      return getNasaStuff().then(function(data) {
+        console.log(data);
+        message.channel.send({
+          embed: {
+            color: 3447003,
+            title: data.title,
+            image: {
+              url: data.hdurl
+            },
+            description: data.explanation,
+            timestamp: new Date(),
+            footer: {
+              text: "Aspen Rules"
+            }
+          }
         });
+      });
 
-        case "surviv-rank":
-            return survivRank(message);
+    case "ip":
+      return myIp().then(function(result) {
+        console.log(result.data.ip);
+        message.author.send(result.data.ip);
+      });
+    case "door":
+      return openDoor().then(function() {
+        message.channel.send("Abrete Sésamo");
+      });
 
-        case "surviv-est":
-            return survivEst(args[1],message);
+    case "surviv-rank":
+      return survivRank(message);
 
-        case "surviv":
-            return surviv();
+    case "surviv-est":
+      return survivEst(args[1], message);
 
-        case "bye":
-            message.channel.send("live long and prosper");
-            break;
+    case "surviv":
+      return surviv();
 
-        case "play":
-            return addSong(args[1], message);
+    case "bye":
+      message.channel.send("live long and prosper");
+      break;
 
-        case "skip":
-            var server = servers[message.guild.id];
-            if (server.dispatcher) server.dispatcher.end();
-            break;
-        case "stop":
-            var server = servers[message.guild.id];
-            if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
-            break;
-          
-        case "test":
-            return testFuckinEverything(message);
-        default:
-            message.channel.send("escribe bien imbecil");
-    }
+    case "play":
+      return addSong(args[1], message);
 
+    case "skip":
+      var server = servers[message.guild.id];
+      if (server.dispatcher) server.dispatcher.end();
+      break;
+    case "stop":
+      var server = servers[message.guild.id];
+      if (message.guild.voiceConnection)
+        message.guild.voiceConnection.disconnect();
+      break;
+
+    case "test":
+      return testFuckinEverything(message);
+    default:
+      message.channel.send("escribe bien imbecil");
+  }
 });
 
-function testFuckinEverything(message){
-    return;
+function testFuckinEverything(message) {
+  return;
 }
 
 function play(connection, message) {
-    var server = servers[message.guild.id];
-    server.dispatcher = connection.playStream(YTDL(server.queue[0], { quality: "highestaudio" }));
-    server.queue.shift();
-    server.dispatcher.on("end", function () {
-        if (server.queue[0]) play(connection, message);
-        else connection.disconnect();
-    })
+  var server = servers[message.guild.id];
+  server.dispatcher = connection.playStream(
+    YTDL(server.queue[0], { quality: "highestaudio" })
+  );
+  server.queue.shift();
+  server.dispatcher.on("end", function() {
+    if (server.queue[0]) play(connection, message);
+    else connection.disconnect();
+  });
 }
 
-function toggleLight(){
-    return httpClient.get("http://181.164.95.251:9891/toggle",);
+function toggleLight() {
+  return httpClient.get("http://181.164.95.251:9891/toggle");
 }
 
 function myIp() {
-    return axios.get('https://api.ipify.org?format=json',{ headers: {'x-forwarded-for': '1.1.1.1'}});
+//   return axios.get("https://api.ipify.org?format=json", {
+//     headers: { "x-forwarded-for": "1.1.1.1" }
+//   });
+return axios.get("https://api.ipify.org?format=json");
 }
+
 function openDoor() {
-    return axios
-    .get('http://192.168.86.37:9891/bell',{ headers: {'x-forwarded-for': '1.1.1.1'}});
+//   return axios.get("http://192.168.86.37:9891/bell");
+    return new Promise();
 }
 
 function getNasaStuff(message) {
-    return httpClient.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY');
-};
-
-function survivRank(message){
-    var users = ["aspen", "zeep", "rakuraku"];
-    survivClient.getSurvivRank(users).then(function (value){message.channel.send(value)});
+  return httpClient.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY");
 }
-function survivEst(user,message){
-    if (!user) {
-        message.channel.send("Ingresar usuario");
-        return;
-    }
-    return survivClient.getSurvivEst(user).then(value => {
-        message.channel.send(value.username + " wins: " + value.wins
-            + " kills: " + value.kills + " games: " + value.games + " kpg: " + value.kpg);
+
+function survivRank(message) {
+  var users = ["aspen", "zeep", "rakuraku"];
+  survivClient.getSurvivRank(users).then(function(value) {
+    message.channel.send(value);
+  });
+}
+function survivEst(user, message) {
+  if (!user) {
+    message.channel.send("Ingresar usuario");
+    return;
+  }
+  return survivClient.getSurvivEst(user).then(value => {
+    message.channel.send(
+      value.username +
+        " wins: " +
+        value.wins +
+        " kills: " +
+        value.kills +
+        " games: " +
+        value.games +
+        " kpg: " +
+        value.kpg
+    );
+  });
+}
+
+function surviv() {
+  survivClient.connect();
+}
+
+function addSong(song, message) {
+  if (!song) {
+    message.channel.send("link missing");
+    return;
+  }
+  if (!message.member.voiceChannel) {
+    message.channel.send("you must be in voice channel K-po");
+    return;
+  }
+  if (!servers[message.guild.id]) {
+    servers[message.guild.id] = {
+      queue: []
+    };
+  }
+  var server = servers[message.guild.id];
+  server.queue.push(song);
+
+  if (!message.guild.voiceConnection) {
+    message.member.voiceChannel.join().then(function(connection) {
+      play(connection, message);
     });
-}
-
-function surviv(){
-    survivClient.connect();
-}
-
-function addSong(song,message){
-    if (!song) {
-        message.channel.send("link missing");
-        return;
-    }
-    if (!message.member.voiceChannel) {
-        message.channel.send("you must be in voice channel K-po");
-        return;
-    }
-    if (!servers[message.guild.id]) {
-        servers[message.guild.id] = {
-            queue: []
-        }
-    }
-    var server = servers[message.guild.id];
-    server.queue.push(song);
-
-    if (!message.guild.voiceConnection) {
-        message.member.voiceChannel.join().then(function (connection) {
-            play(connection, message);
-        })
-    }
+  }
 }
 exports.botClient = botClient;
